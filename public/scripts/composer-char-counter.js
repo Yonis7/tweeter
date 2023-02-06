183f5f07dbdb9042b3dc92ffd088bbdcc53001ee
Yonis7 <yonisaden7@gmail.com>
2023-02-06 06:02:58 +0000

    Dynamic Tweets

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -14,3 +14,47 @@ $(document).ready(function() {
     }
   });
 });
+
+
+function createTweetElement(tweetData) {
+  const { user, content, created_at } = tweetData;
+  const $tweet = $(`
+    <article class="tweet">
+      <header>
+        <img src="${user.avatars}" alt="${user.name} avatar">
+        <h2>${user.name}</h2>
+        <span>${user.handle}</span>
+      </header>
+      <section>
+        <p>${content.text}</p>
+      </section>
+      <footer>
+        <time>${new Date(created_at).toLocaleString()}</time>
+        <div class="icons">
+          <i class="fa fa-flag"></i>
+          <i class="fa fa-retweet"></i>
+          <i class="fa fa-heart"></i>
+        </div>
+      </footer>
+    </article>
+  `);
+
+  return $tweet;
+}
+
+const renderTweets = function(tweets) {
+  $('#tweets-container').empty();
+  for (let tweet of tweets) {
+  let $tweet = createTweetElement(tweet);
+  $('#tweets-container').append($tweet);
+  }
+  };
+  
+  const createTweetElement = function(tweet) {
+  const { user, content, created_at } = tweet;
+  const $tweet = $( <article class="tweet"> <header> <h2>${user.name}</h2> <span>${user.handle}</span> </header> <section> <p>${content.text}</p> </section> <footer> <time>${new Date(created_at).toLocaleString()}</time> <div class="icons"> <i class="fa fa-flag"></i> <i class="fa fa-retweet"></i> <i class="fa fa-heart"></i> </div> </footer> </article> );
+  
+  return $tweet;
+  };
+  
+  renderTweets(data);
